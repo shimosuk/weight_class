891039220f8386ca76f13cb56fce73911487d752
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-08-22 01:21:26 +0900

    スーパーフライ級からスーパーライト級まで追加

--- pro_weight.rb
+++ pro_weight.rb
@@ -8,6 +8,20 @@ def weight_class(kilograms)
     "light fly"
   when 48.97..50.80
     "fly"
+  when 50.80..52.16
+    "super fly"
+  when 52.16..53.52
+    "bantam"
+  when 53.52..55.34
+    "super bantam"
+  when 55.34..57.15
+    "feather"
+  when 57.15..58.97
+    "super feather"
+  when 58.97..61.23
+    "light"
+  when 61.23..63.50
+    "super light"
   else
     "heavy"
   end
--- pro_weight_spec.rb
+++ pro_weight_spec.rb
@@ -15,6 +15,27 @@ describe "test_get_class" do
     weight_class(50.80).should == "fly"
   end
   specify do
+    weight_class(51).should == "super fly"
+  end
+  specify do
+    weight_class(53).should == "bantam"
+  end
+  specify do
+    weight_class(54).should == "super bantam"
+  end
+  specify do
+    weight_class(56).should == "feather"
+  end
+  specify do
+    weight_class(58).should == "super feather"
+  end
+  specify do
+    weight_class(59).should == "light"
+  end
+  specify do
+    weight_class(62).should == "super light"
+  end
+  specify do
     weight_class(86.18).should == "heavy"
   end
 end
